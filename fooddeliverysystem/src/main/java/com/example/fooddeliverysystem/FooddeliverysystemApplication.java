b5b232f20cfbcfb9cfd72f806df7e47ed6d1f0ae
ishach003 <choudharyisha2003@gmail.com>
2026-03-25 16:54:20 +0530

    Add database connectivity in application.yaml

--- fooddeliverysystem/src/main/java/com/example/fooddeliverysystem/FooddeliverysystemApplication.java
+++ fooddeliverysystem/src/main/java/com/example/fooddeliverysystem/FooddeliverysystemApplication.java
@@ -1,5 +1,7 @@
 package com.example.fooddeliverysystem;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +9,16 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class FooddeliverysystemApplication {
 
 	public static void main(String[] args) {
+
+		Dotenv dotenv = Dotenv.configure()
+				.ignoreIfMissing()
+				.load();
+
+		// Set system properties for Spring Boot
+		System.setProperty("DB_URL", dotenv.get("DB_URL"));
+		System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
+		System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
+
 		SpringApplication.run(FooddeliverysystemApplication.class, args);
 	}
 
