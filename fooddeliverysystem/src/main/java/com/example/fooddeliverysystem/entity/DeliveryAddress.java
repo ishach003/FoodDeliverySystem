17edc896778d395390b19cd7d908b01b04b2ad9e
Harshita <harshitagahlot05@gmail.com>
2026-03-27 14:21:20 +0530

    created delivery address and delivery driver entity

--- fooddeliverysystem/src/main/java/com/example/fooddeliverysystem/entity/DeliveryAddress.java
+++ fooddeliverysystem/src/main/java/com/example/fooddeliverysystem/entity/DeliveryAddress.java
@@ -1,4 +1,27 @@
 package com.example.fooddeliverysystem.entity;
 
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Entity
+@Table(name = "DeliveryAddresses")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+
 public class DeliveryAddress {
+    @Id
+    @Column(name = "address_id")
+    private int addressId;
+
+    @Column(name = "address_line")
+    private String addressLine;
+
+    @ManyToOne
+    @JoinColumn(name = "customer_id")
+    private Customer customer;
 }
--- fooddeliverysystem/src/main/java/com/example/fooddeliverysystem/entity/DeliveryDriver.java
+++ fooddeliverysystem/src/main/java/com/example/fooddeliverysystem/entity/DeliveryDriver.java
@@ -1,4 +1,28 @@
 package com.example.fooddeliverysystem.entity;
 
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "DeliveryDrivers")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+
 public class DeliveryDriver {
+    @Id
+    @Column(name = "driver_id")
+    private int driverId;
+
+    @Column(name = "driver_name")
+    private String driverName;
+
+    @Column(name = "driver_phone")
+    private String driverPhone;
 }
