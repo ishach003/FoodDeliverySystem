d43a0e0758a04dc7de71139a63f3f9d340df8433
ishach003 <choudharyisha2003@gmail.com>
2026-03-29 23:55:31 +0530

    Created CouponDto,CustomerAddressDto,CustomerDto

--- fooddeliverysystem/src/main/java/com/example/fooddeliverysystem/dto/CustomerDto.java
+++ fooddeliverysystem/src/main/java/com/example/fooddeliverysystem/dto/CustomerDto.java
@@ -1,4 +1,14 @@
 package com.example.fooddeliverysystem.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class CustomerDto {
+    private String customerName;
+    private String customerEmail;
+    private String customerPhone;
 }
