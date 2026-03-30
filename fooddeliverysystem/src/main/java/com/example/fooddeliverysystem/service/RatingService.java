88739c39deb071da0d1dfb39ceebe55bda4f9382
Shubhi-Awasthi-dev <133631068+Shubhi-Awasthi-dev@users.noreply.github.com>
2026-03-30 14:51:01 +0530

    Created Rating of restaurant successfully

--- fooddeliverysystem/src/main/java/com/example/fooddeliverysystem/service/RatingService.java
+++ fooddeliverysystem/src/main/java/com/example/fooddeliverysystem/service/RatingService.java
@@ -3,6 +3,10 @@ package com.example.fooddeliverysystem.service;
 
 import com.example.fooddeliverysystem.dto.RatingDto;
 
+import java.util.List;
+
 public interface RatingService {
     RatingDto getRatingById(Integer ratingId);
+    List<RatingDto> getRatingsByRestaurant(Integer restaurantId);
+
 }
