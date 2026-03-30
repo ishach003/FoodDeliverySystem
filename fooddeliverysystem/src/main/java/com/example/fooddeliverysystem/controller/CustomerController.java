63c7676f68415a806ec241510b2cc6ea83542d9b
ishach003 <choudharyisha2003@gmail.com>
2026-03-30 11:04:38 +0530

    Fix controller Name

--- fooddeliverysystem/src/main/java/com/example/fooddeliverysystem/controller/CustomerController.java
+++ fooddeliverysystem/src/main/java/com/example/fooddeliverysystem/controller/CustomerController.java
@@ -17,7 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/customer")
 
-public class CustomController {
+public class CustomerController {
 
     private final CustomerService customerService;
 
