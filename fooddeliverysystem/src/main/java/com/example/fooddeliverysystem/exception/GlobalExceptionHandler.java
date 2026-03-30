3a35d0a22c4990acc46f978c4a31087c2eba672e
ishach003 <choudharyisha2003@gmail.com>
2026-03-30 10:52:04 +0530

    created get api find customer by id

--- fooddeliverysystem/src/main/java/com/example/fooddeliverysystem/exception/GlobalExceptionHandler.java
+++ fooddeliverysystem/src/main/java/com/example/fooddeliverysystem/exception/GlobalExceptionHandler.java
@@ -1,4 +1,27 @@
 package com.example.fooddeliverysystem.exception;
 
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import javax.security.sasl.AuthenticationException;
+
+@RestControllerAdvice
 public class GlobalExceptionHandler {
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<ErrorResponse> handleNotFound(
+            ResourceNotFoundException ex, HttpServletRequest req) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                .body(new ErrorResponse(404, "Not Found", ex.getMessage(), req.getRequestURI()));
+    }
+
+    @ExceptionHandler({UnauthorizedException.class, AuthenticationException.class})
+    public ResponseEntity<ErrorResponse> handleUnauthorized(
+            Exception ex, HttpServletRequest req) {
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
+                .body(new ErrorResponse(401, "Unauthorized", ex.getMessage(), req.getRequestURI()));
+    }
+
 }
