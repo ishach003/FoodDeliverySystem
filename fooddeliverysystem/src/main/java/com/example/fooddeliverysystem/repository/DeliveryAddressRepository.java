ad6fd48a8a5b01f6e9fc29799e4bd0b1f953ded2
ishach003 <choudharyisha2003@gmail.com>
2026-03-30 14:36:05 +0530

    Created api get dilveryaddress by customerId

--- fooddeliverysystem/src/main/java/com/example/fooddeliverysystem/repository/DeliveryAddressRepository.java
+++ fooddeliverysystem/src/main/java/com/example/fooddeliverysystem/repository/DeliveryAddressRepository.java
@@ -1,4 +1,13 @@
 package com.example.fooddeliverysystem.repository;
 
-public interface DeliveryAddressRepository {
+import com.example.fooddeliverysystem.entity.DeliveryAddress;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+
+@Repository
+public interface DeliveryAddressRepository extends JpaRepository<DeliveryAddress,Integer> {
+    List<DeliveryAddress> findByCustomer_CustomerId(Integer customerId);
 }
--- fooddeliverysystem/src/main/java/com/example/fooddeliverysystem/service/impl/CustomerServiceImpl.java
+++ fooddeliverysystem/src/main/java/com/example/fooddeliverysystem/service/impl/CustomerServiceImpl.java
@@ -1,15 +1,19 @@
 package com.example.fooddeliverysystem.service.impl;
 
 import com.example.fooddeliverysystem.config.CustomMapper;
+import com.example.fooddeliverysystem.dto.CustomerAddressDto;
 import com.example.fooddeliverysystem.dto.CustomerDto;
 import com.example.fooddeliverysystem.entity.Customer;
 import com.example.fooddeliverysystem.exception.ResourceNotFoundException;
 import com.example.fooddeliverysystem.repository.CustomerRepository;
+import com.example.fooddeliverysystem.repository.DeliveryAddressRepository;
 import com.example.fooddeliverysystem.service.CustomerService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Slf4j
 @Service
@@ -17,6 +21,7 @@ import org.springframework.transaction.annotation.Transactional;
 @Transactional(readOnly = true)
 public class CustomerServiceImpl implements CustomerService {
     private  final CustomerRepository customerRepository;
+    private  final DeliveryAddressRepository addressRepository;
 
     @Override
     public CustomerDto getCustomerById(Integer customerId) {
@@ -24,5 +29,16 @@ public class CustomerServiceImpl implements CustomerService {
         Customer customer=customerRepository.findById(customerId)
                 .orElseThrow(()->new ResourceNotFoundException("Customer",customerId));
         return CustomMapper.customerToCustomerDto(customer);
+    }
+
+    @Override
+    public List<CustomerAddressDto> getCustomerAddress(Integer customerId) {
+       log.info("Fetching customer addresses by customer Id :{}",customerId);
+       customerRepository.findById(customerId)
+               .orElseThrow(()->new ResourceNotFoundException("Customer",customerId));
+       return addressRepository.findByCustomer_CustomerId(customerId)
+               .stream()
+               .map(CustomMapper::addressToDTO)
+               .toList();
     }
 }
