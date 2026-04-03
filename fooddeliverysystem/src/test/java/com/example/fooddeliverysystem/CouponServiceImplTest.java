2dff2e8a9df6166d16dba2133f95880f88a83806
ishach003 <choudharyisha2003@gmail.com>
2026-04-03 11:54:35 +0530

    TestCase for api-getOrderById

--- fooddeliverysystem/src/test/java/com/example/fooddeliverysystem/CouponServiceImplTest.java
+++ fooddeliverysystem/src/test/java/com/example/fooddeliverysystem/CouponServiceImplTest.java
@@ -2,11 +2,13 @@ package com.example.fooddeliverysystem;
 
 import com.example.fooddeliverysystem.dto.CouponDto;
 import com.example.fooddeliverysystem.entity.Coupon;
+import com.example.fooddeliverysystem.exception.ResourceNotFoundException;
 import com.example.fooddeliverysystem.repository.CouponRepository;
 import com.example.fooddeliverysystem.service.impl.CouponServiceImpl;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 
@@ -17,8 +19,10 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import java.time.LocalDate;
 import java.util.List;
 import java.math.BigDecimal;
+import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
@@ -50,42 +54,85 @@ public class CouponServiceImplTest {
         );
     }
 
-    @Test
-    @DisplayName("POSITIVE TestCase for Active Coupon")
-    void positive_returnsActiveCouponDTOList() {
+    @Nested
+    @DisplayName("API  — getCouponById()")
+    class GetCouponById {
 
-        when(couponRepository.findActiveCoupons())
-                .thenReturn(List.of(activeCoupon, anotherActiveCoupon));
+        @Test
+        @DisplayName("PositiveTest")
+        void positiveTestGetCouponById() {
 
-        List<CouponDto> result = couponService.getActiveCoupons();
+            when(couponRepository.findById(1))
+                    .thenReturn(Optional.of(activeCoupon));
 
-        assertThat(result).hasSize(2);
+            CouponDto result = couponService.getCouponById(1);
 
-        assertThat(result.get(0).getCouponId()).isEqualTo(1);
-        assertThat(result.get(0).getCouponCode()).isEqualTo("SAVE20");
-        assertThat(result.get(0).getDiscountAmount())
-                .isEqualByComparingTo(BigDecimal.valueOf(20.0));
-        assertThat(result.get(0).getExpiryDate()).isAfter(LocalDate.now());
+            assertThat(result).isNotNull();
+            assertThat(result.getCouponId()).isEqualTo(1);
+            assertThat(result.getCouponCode()).isEqualTo("SAVE20");
 
-        assertThat(result.get(1).getCouponId()).isEqualTo(2);
-        assertThat(result.get(1).getCouponCode()).isEqualTo("DEAL10");
-        assertThat(result.get(1).getDiscountAmount())
-                .isEqualByComparingTo(BigDecimal.valueOf(10.0));
+            assertThat(result.getDiscountAmount())
+                    .isEqualByComparingTo("20.0");
 
-        verify(couponRepository, times(1)).findActiveCoupons();
-    }
+            assertThat(result.getExpiryDate())
+                    .isAfter(LocalDate.now());
 
-    @Test
-    @DisplayName("NEGATIVE :-TestCase For Non-Active Coupon")
-    void negative_returnsEmptyListWhenNoActiveCoupons() {
+            verify(couponRepository, times(1)).findById(1);
+        }
 
-        when(couponRepository.findActiveCoupons()).thenReturn(List.of());
+        @Test
+        @DisplayName("NegativeTest")
+        void negativeTestGetcouponById() {
 
-        List<CouponDto> result = couponService.getActiveCoupons();
+            when(couponRepository.findById(99))
+                    .thenReturn(Optional.empty());
 
-        assertThat(result).isNotNull();
-        assertThat(result).isEmpty();
+            assertThatThrownBy(() -> couponService.getCouponById(99))
+                    .isInstanceOf(ResourceNotFoundException.class)
+                    .hasMessageContaining("Coupon")
+                    .hasMessageContaining("99");
 
-        verify(couponRepository, times(1)).findActiveCoupons();
+            verify(couponRepository, times(1)).findById(99);
+        }
+
+
+        @Test
+        @DisplayName("POSITIVE TestCase for Active Coupon")
+        void positive_returnsActiveCouponDTOList() {
+
+            when(couponRepository.findActiveCoupons())
+                    .thenReturn(List.of(activeCoupon, anotherActiveCoupon));
+
+            List<CouponDto> result = couponService.getActiveCoupons();
+
+            assertThat(result).hasSize(2);
+
+            assertThat(result.get(0).getCouponId()).isEqualTo(1);
+            assertThat(result.get(0).getCouponCode()).isEqualTo("SAVE20");
+            assertThat(result.get(0).getDiscountAmount())
+                    .isEqualByComparingTo(BigDecimal.valueOf(20.0));
+            assertThat(result.get(0).getExpiryDate()).isAfter(LocalDate.now());
+
+            assertThat(result.get(1).getCouponId()).isEqualTo(2);
+            assertThat(result.get(1).getCouponCode()).isEqualTo("DEAL10");
+            assertThat(result.get(1).getDiscountAmount())
+                    .isEqualByComparingTo(BigDecimal.valueOf(10.0));
+
+            verify(couponRepository, times(1)).findActiveCoupons();
+        }
+
+        @Test
+        @DisplayName("NEGATIVE :-TestCase For Non-Active Coupon")
+        void negative_returnsEmptyListWhenNoActiveCoupons() {
+
+            when(couponRepository.findActiveCoupons()).thenReturn(List.of());
+
+            List<CouponDto> result = couponService.getActiveCoupons();
+
+            assertThat(result).isNotNull();
+            assertThat(result).isEmpty();
+
+            verify(couponRepository, times(1)).findActiveCoupons();
+        }
     }
 }
